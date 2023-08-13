cd39d98df13b1c63e04086c42324266897a675d7
Your Name <poyrazmen35@gmail.com>
2023-08-13 18:52:04 +0300

    Sum of numbers between 1 and 20 that can be divided by 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
-string? word = Console.ReadLine();
-Console.WriteLine(word);
+﻿int answer = 0;
+for (int number = 1; number < 21; number++)
+{
+    if (number % 3 == 0)
+    {
+        Console.WriteLine($"{number} is divisible by 3");
+        answer += number;
+    }
+    else
+    {
+        Console.WriteLine($"{number} is not divisible by 3");
+    }
+}
+Console.WriteLine($"The answer is {answer}");
